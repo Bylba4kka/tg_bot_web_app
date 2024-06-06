19df8ae8cfeb9357735136dba8dde6d1edf7d90d
Aleksey Kozlovskiy <lesha.kozlovskiy.2005@mail.ru>
2024-06-06 15:17:04 +0300

    fix bot. Add README

---
+++ README.md
@@ -0,0 +1,10 @@
+## Простой веб апп в телеграмм боте.
+Заполние формы на сайте и обработка этих данных в боте.
+
+![alt text](image.png)
+
+![alt text](image-1.png)
+
+![alt text](image-2.png)
+
+![alt text](image-3.png)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import asyncio
 import json
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.types import WebAppInfo
-from aiogram.filters import Command, CommandStart, StateFilter, or_f
+from aiogram.filters import CommandStart
+from aiogram.enums import ContentType
 
 
 from config import TOKEN
@@ -17,13 +18,13 @@ dp = Dispatcher()
 async def start(message: types.Message):
     markup = types.ReplyKeyboardMarkup(keyboard=[
         [types.KeyboardButton(text='Открыть Веб страницу', web_app=WebAppInfo(url='https://bylba4kka.github.io/tg_bot_web_app'))]
-    ])
+    ], resize_keyboard=True)
     await message.answer("Привет!", reply_markup=markup)
 
 
-@dp.message(content_types=['web_app_data'])
+@dp.message(F.content_type == ContentType.WEB_APP_DATA)
 async def web_app(message: types.Message):
-    res = json.load(message.web_app_data.data)
+    res = json.loads(message.web_app_data.data)
     await message.answer(f"Имя: {res['name']}\nПочта: {res['email']}\nТелефон\n{res['phone']}")
 
 
